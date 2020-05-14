bb060ff5f2760811b24fdb184b5b673a9daa2869
Ian Halpern <ian.h.halpern@gmail.com>
2020-05-14 10:02:13 -0400

    work on android payload-java wrapper

---
+++ README.md
@@ -0,0 +1,96 @@
+# Payload Android Library [BETA]
+
+An Android library for integrating [Payload](https://payload.co).
+
+## Installation
+
+Download the [latest](https://github.com/payload-code/payload-android/archive/master.zip)
+version from GitHub.
+
+### 2) Include in Project
+
+Include the folder in your Android Studio project as a module.
+
+## Get Started
+
+Once you've included the Payload Java library in your project,
+include the `com.payload.pl` namespace to get started.
+
+All Payload objects and methods are accessible using the `pl` static class.
+
+For more information, please refer to the [Payload Java Libaray](https://github.com/payload-code/payload-java).
+
+### API Authentication
+
+To authenticate with the Payload API, you'll need a live or test API key. API
+keys are accessible from within the Payload dashboard.
+
+```java
+import com.payload.pl;
+
+pl.api_key = "client_key_3bW9JMZtPVDOfFNzwRdfE";
+```
+
+### Creating an Object
+
+
+Interfacing with the Payload API is done primarily through Payload Objects.
+The Payload Android library extends helper functions to simplify Android's
+restrictions on HTTP requests originating from the main thread.
+Below is an example of
+creating a payment using the `pl.Payment` object with the Payload Android helper functions.
+
+
+```java
+// Create a Payment
+Payload.create(new pl.Payment(){{
+    set("amount", 100.0);
+    set("payment_method", new pl.Card(){{
+        set("card_number, "4242 4242 4242 4242");
+    }});
+}}).then((pl.Payment payment) -> {
+    if (payment.getStr("status") == "processed")
+        handleSuccess(payment);
+});
+```
+
+### Void a Payment
+
+```java
+// Create a Payment
+Payload.update(payment, pl.attr("status", "voided") )
+    .then(((pl.Payment voided) -> {
+        handleVoid(payment);
+    });
+```
+
+### Select Customers
+
+```java
+Payload.all(pl.Customer.filter_by(
+    pl.attr("email").eq("matt.perez@example.com")
+)).then((List<pl.Customer> custs) -> {
+    handleCustomers(customers);
+});
+```
+
+### Get a Specific Customer
+
+```java
+Payload.get(pl.Customer.class, cust_id).then((pl.Customer cust) -> {
+    handleCustomer(cust);
+});
+```
+
+### Handle Request Errors
+
+```java
+Payload.create(new pl.Customer(){{
+    set("email", "matt perez@example.com");
+    set("name", "Matt Perez");
+}}).then((pl.Customer cust) -> {
+    handleNewCust(cust);
+}).error((Exceptions.PayloadError error) -> {
+    handleError(error);
+});
+```
---
+++ payload_android/src/main/java/com/payload/android/Payload.java
@@ -0,0 +1,170 @@
+package com.payload.android;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.lang.reflect.Method;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.*;
+
+import com.payload.arm.ARMRequest;
+import com.payload.arm.ARMObject;
+
+public class Payload {
+    private final static String TAG = Payload.class.getSimpleName();
+
+    public interface Success<T> {
+        void callback(T resp);
+    }
+
+    public interface Error {
+        void callback(Throwable err);
+    }
+
+    public static class Promise {
+        Req req;
+
+        public Promise(Req req) {
+            this.req = req;
+        }
+
+        public Promise then(final Success<?> callback) {
+            this.req.success = callback;
+            return this;
+        }
+
+        public Promise error(final Error callback) {
+            this.req.error = callback;
+            return this;
+        }
+
+    }
+
+    private static class Req {
+        ARMObject obj      = null;
+        ARMRequest request = null;
+        Class cls          = null;
+        String action      = null;
+        Success success    = null;
+        Error error        = null;
+        Object resp        = null;
+        Throwable exc      = null;
+        Object arg         = null;
+    }
+
+    static public Promise all(final ARMRequest request) {
+        Req req = new Req();
+        req.request = request;
+        req.action = "all";
+        new Payload.Run().execute(req);
+
+        return new Promise(req);
+    }
+
+
+    static public Promise get(final Class cls, String id) {
+        Req req = new Req();
+        req.cls = cls;
+        req.action = "get";
+        req.arg = id;
+        new Payload.Run().execute(req);
+
+        return new Promise(req);
+    }
+
+    static public Promise create(final ARMObject obj) {
+        Req req = new Req();
+        req.obj = obj;
+        req.action = "create";
+        new Payload.Run().execute(req);
+
+        return new Promise(req);
+    }
+
+    static public Promise update(final ARMObject obj, Map.Entry<String,Object>... args) {
+        Req req = new Req();
+        req.obj = obj;
+        req.action = "update";
+        req.arg = args;
+        new Payload.Run().execute(req);
+
+        return new Promise(req);
+    }
+
+    static public Promise delete(final ARMObject obj) {
+        Req req = new Req();
+        req.obj = obj;
+        req.action = "delete";
+        new Payload.Run().execute(req);
+
+        return new Promise(req);
+    }
+
+    private static class Run extends AsyncTask<Req, Void, Req> {
+        @Override
+        protected Req doInBackground(Req... requests) {
+            Req req = requests[0];
+
+            try {
+                Method action = null;
+                Object data   = null;
+
+                if (req.cls != null) {
+                    action = Stream.of(req.cls.getMethods())
+                        .filter((m) -> m.getName().equals(req.action))
+                        .findFirst()
+                        .get();
+                    //action = req.obj.getClass().getMethod(req.action);
+                    if (req.arg != null)
+                        data = action.invoke(req.cls, req.arg);
+                    else
+                        data = action.invoke(req.obj);
+                } else if (req.obj != null) {
+                    action = Stream.of(req.obj.getClass().getMethods())
+                        .filter((m) -> m.getName().equals(req.action))
+                        .findFirst()
+                        .get();
+                    //action = req.obj.getClass().getMethod(req.action);
+                    if (req.arg != null)
+                        data = action.invoke(req.obj, req.arg);
+                    else
+                        data = action.invoke(req.obj);
+                } else {
+                    action = Stream.of(req.request.getClass().getMethods())
+                        .filter((m) -> m.getName().equals(req.action))
+                        .findFirst()
+                        .get();
+
+                    //action = req.request.getClass().getMethod(req.action);
+                    if (req.arg != null)
+                        data = action.invoke(req.request, req.arg);
+                    else
+                        data = action.invoke(req.request);
+                }
+
+                final Object resp = data;
+                req.resp = resp;
+                return req;
+            } catch ( Exception exc ) {
+                req.exc = exc.getCause();
+                return req;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Req req) {
+            if ( req.exc != null ) {
+                if ( req.error != null )
+                    req.error.callback(req.exc);
+                else
+                    Log.e(TAG, "exception", req.exc);
+            } else if( ARMObject.class.isAssignableFrom(req.resp.getClass()) ) {
+                req.success.callback((ARMObject) req.resp);
+            } else {
+                req.success.callback((List<ARMObject>) req.resp);
+            }
+        }
+
+    }
+}
